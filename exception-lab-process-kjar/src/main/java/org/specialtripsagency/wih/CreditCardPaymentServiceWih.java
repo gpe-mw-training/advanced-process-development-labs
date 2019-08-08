1b0195b9cdd2b1a694d7e616df8ce119912ab323
diego-torres <dtorresf@redhat.com>
2019-08-08 09:34:52 -0300

    test using AbstractException class

--- exception-lab-process-kjar/src/main/java/org/specialtripsagency/wih/CreditCardPaymentServiceWih.java
+++ exception-lab-process-kjar/src/main/java/org/specialtripsagency/wih/CreditCardPaymentServiceWih.java
@@ -3,17 +3,17 @@ package org.specialtripsagency.wih;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jbpm.process.workitem.core.AbstractLogOrThrowWorkItemHandler;
 import org.kie.api.runtime.process.WorkItem;
-import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
 import org.specialtripsagency.PaymentObject;
 
-public class CreditCardPaymentServiceWih implements WorkItemHandler {
+public class CreditCardPaymentServiceWih extends AbstractLogOrThrowWorkItemHandler {
 
     public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
         PaymentObject payment = (PaymentObject) workItem.getParameter("payment");
         if (payment.getCreditCardNumber().startsWith("1234")) {
-            throw new PaymentErrorException("Error with payment for credit card " + payment.getCreditCardHolderName());
+            handleException(new PaymentErrorException("Error with payment for credit card " + payment.getCreditCardHolderName()));
         }
         payment.setStatus("PAID");
         Map<String, Object> results = new HashMap<String, Object>();
