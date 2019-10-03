337207d904b36d0a64e8f5c8494ed528dc2cf90b
diego-torres <dtorresf@redhat.com>
2019-10-02 20:55:07 -0400

    integrate stunner process versions

--- exception-lab-process-kjar/src/main/java/org/specialtripsagency/wih/CreditCardPaymentServiceWih.java
+++ exception-lab-process-kjar/src/main/java/org/specialtripsagency/wih/CreditCardPaymentServiceWih.java
@@ -3,17 +3,17 @@ package org.specialtripsagency.wih;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.jbpm.process.workitem.core.AbstractLogOrThrowWorkItemHandler;
 import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItemManager;
 import org.specialtripsagency.PaymentObject;
 
-public class CreditCardPaymentServiceWih extends AbstractLogOrThrowWorkItemHandler {
+public class CreditCardPaymentServiceWih implements WorkItemHandler {
 
     public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
         PaymentObject payment = (PaymentObject) workItem.getParameter("payment");
         if (payment.getCreditCardNumber().startsWith("1234")) {
-            handleException(new PaymentErrorException("Error with payment for credit card " + payment.getCreditCardHolderName()));
+            throw new PaymentErrorException("Error with payment for credit card " + payment.getCreditCardHolderName());
         }
         payment.setStatus("PAID");
         Map<String, Object> results = new HashMap<String, Object>();
@@ -28,3 +28,4 @@ public class CreditCardPaymentServiceWih extends AbstractLogOrThrowWorkItemHandl
     
 
 }
+
--- exception-lab-process-kjar/src/main/java/org/specialtripsagency/wih/PaymentErrorException.java
+++ exception-lab-process-kjar/src/main/java/org/specialtripsagency/wih/PaymentErrorException.java
@@ -27,3 +27,4 @@ public class PaymentErrorException extends RuntimeException {
     
 
 }
+
